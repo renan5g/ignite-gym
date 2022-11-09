fbb9abcf51c873d5494c36429fd19fdbb12e9afa
renan5g <renansilva.dev5g@gmail.com>
2022-11-09 15:04:02 -0300

    feat: bottom tab

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { Box, useTheme } from 'native-base';
 
+import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Router() {
@@ -11,7 +12,8 @@ export function Router() {
   return (
     <Box flex={1} backgroundColor="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {/* <AuthRoutes /> */}
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
